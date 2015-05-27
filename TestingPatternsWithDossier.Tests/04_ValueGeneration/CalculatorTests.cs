410c9ef1239d3b8d53cb0eb36c738deba93b1ea1
Rob Moore <me@robdmoore.id.au>
2015-05-27 08:49:18 +0800

    Added examples of value generation

---
+++ TestingPatternsWithDossier.Tests/04_ValueGeneration/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Shouldly;
+using TestStack.Dossier;
+using TestStack.Dossier.EquivalenceClasses;
+
+namespace TestingPatternsWithDossier.Tests._04_ValueGeneration
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void ShouldAddSinglePositiveIntegerFromInitialState()
+        {
+            var x = Any.PositiveInteger();
+
+            var result = _calculator.Add(x);
+
+            result.ShouldBe(x);
+        }
+
+        [Test]
+        public void ShouldAddTwoPositiveIntegersConsecutivelyFromInitialState()
+        {
+            var x = Any.PositiveInteger();
+            var y = Any.PositiveInteger();
+            var expected = x + y;
+
+            _calculator.Add(x);
+            var result = _calculator.Add(y);
+
+            result.ShouldBe(expected);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new Calculator();
+        }
+
+        private Calculator _calculator;
+        // ReSharper disable once InconsistentNaming
+        private AnonymousValueFixture Any = new AnonymousValueFixture();
+
+    }
+}
---
+++ TestingPatternsWithDossier/Calculator.cs
@@ -0,0 +1,37 @@
+﻿namespace TestingPatternsWithDossier
+{
+    public class Calculator
+    {
+        private int _currentResult = 0;
+
+        public int Add(int x)
+        {
+            _currentResult += x;
+            return _currentResult;
+        }
+
+        public int Subtract(int x)
+        {
+            _currentResult -= x;
+            return _currentResult;
+        }
+
+        public int Multiply(int x)
+        {
+            _currentResult *= x;
+            return _currentResult;
+        }
+
+        public int Divide(int x)
+        {
+            _currentResult *= x;
+            return _currentResult;
+        }
+
+        public int Clear()
+        {
+            _currentResult = 0;
+            return _currentResult;
+        }
+    }
+}
